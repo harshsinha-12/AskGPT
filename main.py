7f802a45e8959491e1973401a047a060766aff73
Harsh Sinha <sinha.harshsep@gmail.com>
2023-11-10 21:04:41 +0530

    gitignore

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+# main_script.py
 import streamlit as st
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Access the OpenAI API key from the environment
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 st.title('Your Personal Assistant, throw any question at me😉')
 
-openai_api_key = st.sidebar.text_input('Enter your OpenAI API Key')
-
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    text = st.text_area('Enter text:')
+    submitted = st.form_submit_button('Submit')
+
+    if not openai_api_key or not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key in the .env file!', icon='⚠')
+
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
